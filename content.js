922154472d01a4c61961f1d811522a59e15915e1
Yester Selga <yesterselga@gmail.com>
2022-08-09 13:30:02 +0800

    added youtube music

--- content.js
+++ content.js
@@ -1,20 +1,18 @@
 
-window.onload=function(){
-	javascript: (function() {
-	  setInterval(function() {
-		   var $skip1 = document.getElementsByClassName("ytp-ad-skip-button")[0];
-		   if ($skip1 != undefined) {
-			   $skip1.click();
-			   console.log('Ad skipped');
-		   }
-		   
-		   var $skip2 = document.getElementsByClassName("ytp-ad-overlay-close-button")[0];
-		   if ($skip2 != undefined) {
-			   $skip2.click();
-			   console.log('Ad popup closed');
-		   }
-	  }, 1000);
-	  
-	  console.log('YouTube auto skip 1.1 loaded!');
-	})();
+window.onload = function () {
+     javascript: (function () {
+          setInterval(function () {
+               var $skip1 = document.getElementsByClassName("ytp-ad-skip-button")[0];
+               if ($skip1 != undefined) {
+                    $skip1.click();
+               }
+
+               var $skip2 = document.getElementsByClassName("ytp-ad-overlay-close-button")[0];
+               if ($skip2 != undefined) {
+                    $skip2.click();
+               }
+          }, 1000);
+
+          console.log('YouTube auto skip 1.1 loaded!');
+     })();
 }
